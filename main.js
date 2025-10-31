41da810ec2bbf1fd7554f07b33dadab0d3917a25
Aham Kumar <ahamkr2@gmail.com>
2025-10-31 22:33:50 +0530

    product added

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ function menuOpen(){
         menuDiv.style.display="flex"
     }
 }
+
+async function showProduct() {
+    try {
+        let res = await fetch('https://fakestoreapi.com/products')
+        let products = await res.json()
+        console.log(products)
+        products.forEach((product)=>{
+            let div = document.getElementById('product')
+            let box = document.createElement('div')
+            box.innerHTML = `
+                <img src="${product.image}" alt="">
+                <p>${product.title}</p>
+                <h3>₹${product.price}</h3>
+            `
+            div.appendChild(box)
+        })
+    } catch (error) {
+        
+    }
+}
+
+showProduct()
